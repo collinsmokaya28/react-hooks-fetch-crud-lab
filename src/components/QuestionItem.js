fcf90fb2f2947811b84f0f78f00a6b5701444296
+ <+>
2022-09-28 14:34:49 +0300

    Completed assignment

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,7 +1,10 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, delQuestion, updatedQuestion}) {
+
+
   const { id, prompt, answers, correctIndex } = question;
+  console.log('en question' + question)
 
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
@@ -9,15 +12,24 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  function handleDelClick() {
+    delQuestion(id)
+  }
+
+  function handleUpClick(e){
+    const newCorrAns = e.target.value
+    updatedQuestion(id, newCorrAns);
+  }
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onClick={handleUpClick}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelClick}>Delete Question</button>
     </li>
   );
 }
